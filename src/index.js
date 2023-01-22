19e5c6f4f1e3d8fa7beb221ea3b8f04f9c23171f
ScriptAndTonic <hello@scriptandtonic.com>
2023-01-22 21:46:49 +0200

    Cleanup and submit button logic and styling

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ const { generate } = require('rxjs');
 
 const pdfGenerator = require('./pdf_generator/pdf_generator');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 400,
     height: 500,
@@ -18,38 +16,19 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
-  ipcMain.handle('dialog:openDirectory', async () => {
-    const { canceled, filePaths } = await dialog.showOpenDialog(mainWindow, {
-      title: 'Save Folder',
-      message: 'Choose a Folder to Save the generated PDFs',
-      buttonLabel: 'Select',
-      properties: ['openDirectory'],
-    });
-    if (canceled) {
-      return;
-    } else {
-      return filePaths[0];
-    }
-  });
+  ipcMain.handle('dialog:openDirectory', () => openDirectory(mainWindow));
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   ipcMain.on('generate-pdfs', generatePDFs);
+  // ipcMain.on('open-settings-page', openSettingsPage);
   createWindow();
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -57,16 +36,12 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-
+// --- CUSTOM LOGIC STARTS HERE ---
 const generatePDFs = async (event, pdfTemplateFilePath, excelDataFilePath, outputDirectoryPath) => {
   console.log('Main received');
   console.dir(event);
@@ -78,3 +53,27 @@ const generatePDFs = async (event, pdfTemplateFilePath, excelDataFilePath, outpu
   console.log(`Output Directory Path: ${outputDirectoryPath}`);
   await pdfGenerator.generatePDFs(pdfTemplateFilePath, excelDataFilePath, outputDirectoryPath);
 };
+
+const openDirectory = async (parentWindow) => {
+  const { canceled, filePaths } = await dialog.showOpenDialog(parentWindow, {
+    title: 'Save Folder',
+    message: 'Choose a Folder to Save the generated PDFs',
+    buttonLabel: 'Select',
+    properties: ['openDirectory'],
+  });
+  if (canceled) {
+    return;
+  } else {
+    return filePaths[0];
+  }
+};
+
+// const openSettingsPage = async (event) => {
+//   const mainWindow = new BrowserWindow({parent
+//     width: 400,
+//     height: 500,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js'),
+//     },
+//   });
+// };
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('#pdf-generator-form');
 const pdfTemplateFilePicker = document.querySelector('#pdf-template-file-picker');
 const excelDataFilePicker = document.querySelector('#excel-data-file-picker');
+const submitButton = document.querySelector('#submit-btn');
+validateForm();
 
 form.addEventListener('submit', generatePDFs);
 
@@ -9,6 +11,7 @@ pdfTemplateFilePicker.onchange = () => {
     const fileName = document.querySelector('#pdf-template-file-picker-container .file-name');
     fileName.textContent = pdfTemplateFilePicker.files[0].name;
   }
+  validateForm();
 };
 
 excelDataFilePicker.onchange = () => {
@@ -16,17 +19,20 @@ excelDataFilePicker.onchange = () => {
     const fileName = document.querySelector('#excel-data-file-picker-container .file-name');
     fileName.textContent = excelDataFilePicker.files[0].name;
   }
+  validateForm();
 };
+
+function validateForm() {
+  submitButton.disabled = pdfTemplateFilePicker.files.length === 0 || excelDataFilePicker.files.length === 0;
+}
 
 function generatePDFs(e) {
   e.preventDefault();
-  console.log('Renderer received');
   const pdfTemplateFilePath = pdfTemplateFilePicker.files[0].path;
-  console.log(`Template Path: ${pdfTemplateFilePath}`);
   const excelDataFilePath = excelDataFilePicker.files[0].path;
-  console.log(`Data File Path: ${excelDataFilePath}`);
   window.electronAPI.selectFolder().then((outputDirectoryPath) => {
-    console.log(`Output Directory Path: ${outputDirectoryPath}`);
-    window.electronAPI.generatePDFs(pdfTemplateFilePath, excelDataFilePath, outputDirectoryPath);
+    if (pdfTemplateFilePath != undefined && excelDataFilePath != undefined && outputDirectoryPath != undefined) {
+      window.electronAPI.generatePDFs(pdfTemplateFilePath, excelDataFilePath, outputDirectoryPath);
+    }
   });
 }
